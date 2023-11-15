efb086f199df7a8c05da41b13fc418773a46ca22
ibconcept <willima09@gmail.com>
2023-11-15 17:20:33 +0300

    b

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const cardContainer = document.getElementById("cardContainer");
 const imageFilenames = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", 
+    "1", "2", "3", "4",
+    "5", "6", "7", "8",
+    "9", "10", "11", "12",
+    "13", "14", "15", "16",
+    "17", "18", "19", "20",
+    "21", "22", "23", "24",
+    "25", "26", "27", "28",
+    "29", "30", "31", "32",
+    "33", "34", "35", "36",
+    "37", "38", "39", "40",
     // Add more filenames here
 ];
 
@@ -16,7 +25,7 @@ for (let i = 0; i < numberOfCards; i++) {
     card.classList.add("card");
 
     const imageFilename = getRandomImageFilename();
-    const imagePath = `images/${imageFilename}.svg`; // Updated image path
+    const imagePath = `images/${imageFilename}.svg`; 
     const image = document.createElement("img");
     image.src = imagePath;
 
