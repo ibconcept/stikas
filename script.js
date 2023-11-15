97b75b6c3a9e0e8adf1a9b81d6f4da7f95dd11d8
unknown <willima09@gmail.com>
2023-11-15 14:58:55 +0300

    b

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const cardContainer = document.getElementById("cardContainer");
 const imageFilenames = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
-
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", 
     // Add more filenames here
 ];
 
